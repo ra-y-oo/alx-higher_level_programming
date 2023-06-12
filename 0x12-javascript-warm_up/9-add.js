1eeab5c7dff8f9c879471335d555b62f0d1a053a
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-06-12 14:41:10 +0300

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
-function add (a, b) {
-    return a + b;
-  }
-  
-  console.log(add(Number(process.argv[2]), Number(process.argv[3])));
-  
+const int1 = process.argv[2];
+const int2 = process.argv[3];
+
+function add(a, b) {
+    console.log(a + b);
+}
+
+const result = add(int1, int2);
+console.log(result);
