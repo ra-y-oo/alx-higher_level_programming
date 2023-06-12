154621dcc127de14a9aee2af2855bbf96217bc4b
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-06-12 14:39:40 +0300

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,17 +1,7 @@
 #!/usr/bin/node
-const argument = parseInt(process.argv[2]);
-
-function factorial(n) {
-    if (Number.isNaN(n)) {
-        return 1;
-    }
-
-    if (n === 0 || n === 1) {
-        return 1;
-    }
-
-    return n * factorial(n - 1);
-}
-
-const result = factorial(argument);
-console.log(result);
+function factorial (n) {
+    return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+  }
+  
+  console.log(factorial(Number(process.argv[2])));
+  
