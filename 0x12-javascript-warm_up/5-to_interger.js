4353ea3683b3696e820d1cffd574f47fc4481e32
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-06-12 13:58:08 +0300

    Update 5-to_interger.js

--- 0x12-javascript-warm_up/5-to_interger.js
+++ 0x12-javascript-warm_up/5-to_interger.js
@@ -2,7 +2,7 @@
 const [, , argument] = process.argv;
 const integer = parseInt(argument);
 
-if (Number.isNaN(integer)) {
+if (isNaN(integer)) {
     console.log('Not a number');
 } else {
     console.log('My number: ${integer}');
