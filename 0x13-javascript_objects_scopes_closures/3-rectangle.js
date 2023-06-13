16957f1abd4a89cac93e6e36e23d32f2d30b2dff
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-06-13 14:39:57 +0300

    Create 3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      // Create an empty object
+      return {};
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log("X".repeat(this.width));
+      }
+    }
+  }
+}
