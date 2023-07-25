b02641681490293c98099ae6607ab2f98e04f9c9
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-07-25 23:47:23 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-filePath = process.argv[2];
+let filePath = process.argv[2];
 fs.readFile(filePath, 'utf-8', function(error, data) {
     console.log(error || data);
 });
