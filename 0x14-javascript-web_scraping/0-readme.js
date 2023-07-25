8653bc5f9decca963f908a79f78c6270a51881dc
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-07-25 23:57:47 +0300

    Update readme file and readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', function(error, data) {
+
+fs.readFile(process.argv[2], 'utf-8', function (error, data) {
   console.log(error || data);
 });
--- 0x14-javascript-web_scraping/README.md
+++ 0x14-javascript-web_scraping/README.md
@@ -1,1 +1,3 @@
 # 0x14-javascript-web_scraping
+
+[![js-semistandard-style](https://raw.githubusercontent.com/standard/semistandard/master/badge.svg)](https://github.com/standard/semistandard)
