2c07d0d2abb1a0302f77481534c8bf095260944c
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-07-26 04:28:05 +0300

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const { error } = require('console');
 const fs = require('fs');
-fs.writeFileSync(process.argv[2], process.argv[3], error => {
-  if (error) console.error(error);
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
